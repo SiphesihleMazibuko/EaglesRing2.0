4221b1e3f379ce43ac4888665edd36ab5bd36aab
SiphesihleMazibuko <amazibuko763@gmail.com>
2024-07-25 11:21:12 +0200

    Removed the Ongetstarted component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,7 @@ export default function Home() {
   const { user } = useUser();
   const router = useRouter();
 
-  const handleGetStartedClick = () => {
-    setShowOnboarding(false);
-  };
+ 
   
   useEffect(() => {
     if (isSignedIn) {
@@ -31,7 +29,7 @@ export default function Home() {
   return (
     <>
       {showOnboarding ? (
-        <Onboarding onGetStartedClick={handleGetStartedClick} />
+        <Onboarding  />
       ) : (
         <>
           <SignedOut>
