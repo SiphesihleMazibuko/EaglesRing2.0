54585bcb1b307f813ac44517dd44ab2822d27d9b
SiphesihleMazibuko <amazibuko763@gmail.com>
2024-07-25 23:55:18 +0200

    Updated slider and cleaned the code

--- src/app/slider/page.tsx
+++ src/app/slider/page.tsx
@@ -1,20 +1,28 @@
-import React, { useCallback } from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
+import React from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
 
 const Slider = () => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({loop: true}, [Autoplay({delay: 3000, stopOnMouseEnter:true, stopOnInteraction: false })])
+  const autoplayOptions = { delay: 3000, stopOnMouseEnter: true, stopOnInteraction: false };
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay(autoplayOptions)]);
 
   return (
     <div className='embla'>
-    <div className="embla--viewport  mt-12 h-56" ref={emblaRef}>
-      <div className="embla__container h-full">
-        <div className="embla__slide flex items-center justify center"><img src='https://images.pexels.com/photos/1181396/pexels-photo-1181396.jpeg?auto=compress&cs=tinysrgb&w=600' alt='Corporate Meeting'/></div>
-        <div className="embla__slide flex items-center justify center"><img src="https://images.pexels.com/photos/1709003/pexels-photo-1709003.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Bonday Exhibition" /></div>
-        <div className="embla__slide flex items-center justify center"><img src="https://images.pexels.com/photos/716281/pexels-photo-716281.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Global Business Summit" /></div>
+      <div className="embla__viewport mt-12 h-56" ref={emblaRef}>
+        <div className="embla__container h-full">
+          <div className="embla__slide flex items-center justify-center">
+            <img src='https://images.pexels.com/photos/1181396/pexels-photo-1181396.jpeg?auto=compress&cs=tinysrgb&w=600' alt='Corporate Meeting'/>
+          </div>
+          <div className="embla__slide flex items-center justify-center">
+            <img src="https://images.pexels.com/photos/1709003/pexels-photo-1709003.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Bonday Exhibition" />
+          </div>
+          <div className="embla__slide flex items-center justify-center">
+            <img src="https://images.pexels.com/photos/716281/pexels-photo-716281.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Global Business Summit" />
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
-export default Slider
+
+export default Slider;
